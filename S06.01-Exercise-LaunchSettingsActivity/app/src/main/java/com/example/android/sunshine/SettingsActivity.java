0733c25ecd266e050b65ded552a64f20905d413b
vmois <vladyslav.moisieienkov@gmail.com>
2017-12-27 17:00:34 +0100

    S06.01 Add. info

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -23,7 +23,7 @@ public class SettingsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            finish();
+            finish(); // or onBackPressed()
         }
         return super.onOptionsItemSelected(item);
     }
